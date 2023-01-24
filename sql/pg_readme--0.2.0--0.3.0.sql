cf25534e6ebd50b477a35c48e2bd23adcae5c22d
Rowan <rowan@bigsmoke.us>
2023-01-24 17:15:41 +0000

    Backhack the v. 0.2.0 → 0.3.0 update script to not require superuser
    The `.control` file has all this time advertised that this extension
    doesn't require superuser privileges.  So I want back into time (or
    semantic versioning space, if you will) and made it so that this is
    actually true.

--- sql/pg_readme--0.2.0--0.3.0.sql
+++ sql/pg_readme--0.2.0--0.3.0.sql
@@ -4,9 +4,27 @@
 --------------------------------------------------------------------------------------------------------------
 
 do $$
+declare
+    _ddl_cmd_to_set_pg_readme_url text;
 begin
-    execute 'ALTER DATABASE ' || current_database()
+    _ddl_cmd_to_set_pg_readme_url := 'ALTER DATABASE ' || current_database()
         || ' SET pg_readme.readme_url = ''https://github.com/bigsmoke/pg_readme/blob/master/README.md''';
+    execute _ddl_cmd_to_set_pg_readme_url;
+exception
+    when insufficient_privilege then
+        -- We say `superuser = false` in the control file; so let's just whine a little instead of crashing.
+        raise warning using
+            message = format(
+                'Because you''re installing the pg_readme extension as non-superuser and because you'
+                || ' are also not the owner of the %I DB, the database-level `pg_readme.readme_url` setting'
+                || ' has not been set.',
+                current_database()
+            )
+            ,detail = 'Settings of the form `<extension_name>.readme_url` are used by `pg_readme` to'
+                || ' cross-link between extensions their README files.'
+            ,hint = 'If you want full inter-extension README cross-linking, you can ask your friendly'
+                || E' neighbourhood DBA to execute the following statement:\n'
+                || _ddl_cmd_to_set_pg_readme_url || ';';
 end;
 $$;
 
